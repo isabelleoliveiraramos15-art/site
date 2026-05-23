87ade45727e23139c2d8ea312838f58666621e53
isabelleoliveiraramos15-art <isabelleoliveiraramos15@gmail.com>
2026-05-23 10:41:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirLogin(){
+login.style.display="flex"
+}
+
+function fecharLogin(){
+login.style.display="none"
+}
+
+function abrirDetalhes(){
+detalhes.style.display="flex"
+}
+
+function fecharDetalhes(){
+detalhes.style.display="none"
+}
